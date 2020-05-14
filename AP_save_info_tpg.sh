34bf72bf6f4ad9abfe54234151d2a176d06efa3f
ifollowbenoit <benoit.boyeau@ifollow.fr>
2020-05-14 10:22:29 +0200

    saving

---
+++ AP_save_info_tpg.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+INTERFACE="wlp1s0"
+AP1="18:e8:29:c6:0c:0f"
+AP2="18:e8:29:10:b0:8b"
+AP3="18:e8:29:c6:0f:3f"
+AP4="74:83:c2:96:ee:27"
+while [ 1 ];
+do CONNECTED_AP=$(iwconfig $INTERFACE |grep "Access Point" | cut -d ':' -f 4-9 | cut -c 2-);
+
+DIFFERENCE1=$(diff -w  <(echo "$CONNECTED_AP") <(echo "$AP1"));
+DIFFERENCE2=$(diff -w  <(echo "$CONNECTED_AP") <(echo "$AP2"));
+DIFFERENCE3=$(diff -w  <(echo "$CONNECTED_AP") <(echo "$AP3"));
+DIFFERENCE4=$(diff -w  <(echo "$CONNECTED_AP") <(echo "$AP4"));
+
+now=$(date +"%T")
+if [ -z "$DIFFERENCE1" ]
+then
+        echo "AP1:" $now >> log_ap.txt
+elif [ -z "$DIFFERENCE2" ]
+then
+        echo "AP2:" $now >> log_ap.txt
+elif [ -z "$DIFFERENCE3" ]
+then
+        echo "AP3:" $now >> log_ap.txt
+else
+        echo "AP4:" $now >> log_ap.txt
+fi
+sleep 1;
+
+test $? -gt 128 && break;
+done
